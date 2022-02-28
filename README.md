805d74ac9c4b6be2f1c9786b76efa27a08c09443
Zach Rojas <zachrojas94@gmail.com>
2022-02-28 12:58:01 -0600

    adding what I have on this machine, original files lost

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Code-Quiz
+
+This is assignment 04 for the SMU Full Stack Web Development Bootcamp. 
+
+A timed coding quiz that ask you questions about javascript. 
---
+++ script.js
@@ -0,0 +1,26 @@
+// Global variables
+var timer = document.querySelector('#timer');
+var startBtn = document.querySelector('#start-button');
+var viewScores = document.querySelector('#highscores-button');
+var questionsContainer = document.querySelector('#questions');
+var quizQuestions = 
+
+function startGame() {
+    if (startBtn)
+};
+  
+function startTimer() {
+
+};
+
+function nextQuestion() {
+
+};
+
+function selectAnswer() {
+
+};
+
+function generateScore() {
+
+};
